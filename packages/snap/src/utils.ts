03bfc7ddfb97f18af7171ace8c2a1bbb2efbf9c3
Richard Liu <richard.y.liu@berkeley.edu>
2023-03-21 16:08:03 +0700

    lint

--- packages/snap/src/utils.ts
+++ packages/snap/src/utils.ts
@@ -1,3 +1,9 @@
+/**
+ * Returns the abbreviated representation of the Ethereum keccak address, e.g. 0xabc...123.
+ *
+ * @param address - The full-length Ethereum address.
+ * @returns Abbreviated string.
+ */
 export function abbreviateEthereumAddress(address: string) {
   return `${address.slice(0, 5)}...${address.slice(address.length - 3)}`;
 }
